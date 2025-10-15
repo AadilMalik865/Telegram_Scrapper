f636317fae1587c24efb221714497a9ea12cb932
AadilMalik865 <myproject2k25@gmail.com>
2025-10-15 18:07:45 +0000

    Add initial Telegram Scraper code

--- app.py
+++ app.py
@@ -69,4 +69,5 @@ def download(file_name):
         return "File not found!", 404
 
 if __name__ == "__main__":
-    app.run(debug=False, threaded=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="127.0.0.1", port=port, debug=False, threaded=True)
